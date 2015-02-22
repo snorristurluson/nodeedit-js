3e5195b44b14241a06cee84ba131cfcbbf004204
snorri.sturluson <snorri.sturluson@ccpgames.com>
2015-02-22 19:46:39 +0000

    Added buttons to add and remove nodes. Connectors added by alt-dragging between nodes.

--- nodeedit.js
+++ nodeedit.js
@@ -151,12 +151,15 @@ function Node(name, x, y) {
     this.width = 100;
     this.height = 30;
     this.isHighlighted = false;
-    this.connectors = [];
+    this.isSelected = false;
 
     this.render = function (ctx) {
         ctx.fillStyle = "white";
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        if (this.isHighlighted) {
+        if (this.isSelected) {
+            ctx.strokeStyle = "blue";
+        }
+        else if (this.isHighlighted) {
             ctx.strokeStyle = "red";
         }
         else {
@@ -171,13 +174,6 @@ function Node(name, x, y) {
         var halfWidth = this.width / 2;
         var halfHeight = this.height / 2;
         ctx.fillText(this.name, this.x + halfWidth, this.y + halfHeight);
-
-        ctx.lineWidth = 2;
-
-        var i;
-        for (i = 0; i < this.connectors.length; ++i) {
-            this.connectors[i].render(ctx);
-        }
     };
 
     this.isInside = function (x, y) {
@@ -196,14 +192,6 @@ function Node(name, x, y) {
 
         return true;
     };
-
-    this.setHighlight = function (val) {
-        this.isHighlighted = val;
-    };
-
-    this.connectTo = function (node) {
-        this.connectors.push(new Connector(this, node));
-    };
 }
 
 
@@ -217,40 +205,94 @@ function MouseCoordsToCanvas(event) {
 
 function Scene() {
     this.nodes = [];
+    this.connectors = [];
 
     this.nodeUnderCursor = null;
+    this.isDragging = false;
     this.nodeDragged = null;
+    this.connectorDragged = null;
     this.lastMousePos = null;
+    this.selected = null;
 
     this.addNode = function (node) {
         this.nodes[this.nodes.length] = node;
     };
 
+    this.removeNode = function (node) {
+        var ix = this.nodes.indexOf(node);
+        this.nodes.splice(ix, 1);
+        if (node == this.selected) {
+            this.selected = null;
+        }
+        this.removeConnectorsForNode(node);
+    }
+
+    this.removeConnectorsForNode = function (node) {
+        var newConnectors = [];
+        var i;
+        for(i = 0; i < this.connectors.length; ++i) {
+            var connector = this.connectors[i];
+            if (connector.from != node && connector.to != node) {
+                newConnectors.push(connector);
+            }
+        }
+        this.connectors = newConnectors;
+    }
+
+    this.addConnector = function (connector) {
+        this.connectors.push(connector);
+    }
+
     this.render = function (ctx) {
         var i;
-
-        if (this.nodes.length == 0) {
-            return;
-        }
 
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";
+        ctx.fillStyle = "black";
+
+        var i;
+        for (i = 0; i < this.connectors.length; ++i) {
+            this.connectors[i].render(ctx);
+        }
+
+        if (this.connectorDragged) {
+            this.connectorDragged.render(ctx);
+        }
+
         for (i = this.nodes.length - 1; i >= 0; --i) {
             this.nodes[i].render(ctx);
         }
+
     };
 
-    this.onMouseDown = function (x, y) {
+    this.onMouseDown = function (x, y, event) {
+        console.log("mousedown:", x, y, event.altKey);
         var node = this.pick(x, y);
         if (node) {
-            this.nodeDragged = node;
-            var ix = this.nodes.indexOf(node);
-            this.nodes.splice(ix, 1);
-            this.nodes.splice(0, 0, node);
+            if (event.altKey) {
+                this.connectorDragged = new Connector(node, {x: x, y: y, width: 1, height: 1});
+            }
+            else {
+                this.nodeDragged = node;
+                var ix = this.nodes.indexOf(node);
+                if (ix > 0) {
+                    this.nodes.splice(ix, 1);
+                    this.nodes.splice(0, 0, node);
+                }
+                if (this.selected && this.selected != node) {
+                    this.selected.isSelected = false;
+                }
+                node.isSelected = !node.isSelected;
+                if (node.isSelected) {
+                    this.selected = node;
+                }
+            }
         }
 
-        this.render(ctx)
+        this.render(ctx);
     };
 
     this.onMouseMove = function (x, y) {
@@ -263,16 +305,24 @@ function Scene() {
             var dy = y - this.lastMousePos.y;
             this.nodeDragged.x += dx;
             this.nodeDragged.y += dy;
+            this.isDragging = true;
+        }
+        else if (this.connectorDragged) {
+            var to = this.pick(x, y);
+            if (!to) {
+                to = {x: x, y: y, width: 1, height: 1};
+            }
+            this.connectorDragged.to = to;
         }
         else {
             var node = this.pick(x, y);
             if (node != this.nodeUnderCursor) {
                 if (this.nodeUnderCursor) {
-                    this.nodeUnderCursor.setHighlight(false);
+                    this.nodeUnderCursor.isHighlighted = false;
                 }
                 this.nodeUnderCursor = node;
                 if (this.nodeUnderCursor) {
-                    this.nodeUnderCursor.setHighlight(true);
+                    this.nodeUnderCursor.isHighlighted = true;
                 }
             }
         }
@@ -283,6 +333,21 @@ function Scene() {
     };
 
     this.onMouseUp = function (x, y) {
+        if (!this.isDragging && this.nodeDragged && this.nodeDragged.isSelected) {
+            this.nodeDragged.isSelected = false;
+            this.selected = null;
+        }
+
+        if (this.connectorDragged) {
+            var node = this.pick(x, y);
+            if (node && node != this.connectorDragged.from) {
+                this.connectorDragged.to = node;
+                this.connectors.push(this.connectorDragged);
+            }
+            this.connectorDragged = null;
+        }
+
+        this.isDragging = false;
         this.nodeDragged = null;
         this.render(ctx);
     };
@@ -305,29 +370,43 @@ scene = new Scene();
 
 function OnMouseDown(event) {
     var coords = MouseCoordsToCanvas(event);
-    scene.onMouseDown(coords.x, coords.y)
+    scene.onMouseDown(coords.x, coords.y, event)
+    event.preventDefault();
 }
 
 function OnMouseMove(event) {
     var coords = MouseCoordsToCanvas(event);
     scene.onMouseMove(coords.x, coords.y)
+    event.preventDefault();
 }
 
 function OnMouseUp(event) {
     var coords = MouseCoordsToCanvas(event);
     scene.onMouseUp(coords.x, coords.y)
+    event.preventDefault();
 }
 
 canvas.addEventListener("mousedown", OnMouseDown);
 canvas.addEventListener("mousemove", OnMouseMove);
 canvas.addEventListener("mouseup", OnMouseUp);
 
+function onClickAdd() {
+    var name = document.getElementById('name').value;
+    var node = new Node(name, 0, 0)
+    scene.addNode(node);
+    scene.render(ctx);
+}
+
+function onClickDelete() {
+    if (scene.selected) {
+        scene.removeNode(scene.selected);
+        scene.render(ctx);
+    }
+}
+
 var first = new Node("First", 20, 20);
 var second = new Node("Second", 60, 200);
 var third = new Node("Third", 150, 140);
-first.connectTo(second);
-// first.connectTo(third);
-second.connectTo(third);
 scene.addNode(first);
 scene.addNode(second);
 scene.addNode(third);
